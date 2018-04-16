90667b224e54c2600ed4fd39787d81321beac17b
Enzo Soulié <soulie.enzo@gmail.com>
2018-04-16 14:19:52 +0200

    Fix android problem multiple recipients

--- android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
+++ android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
@@ -3,6 +3,7 @@ package com.tkporter.sendsms;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
+import android.net.Uri;
 import android.provider.Telephony;
 
 import com.facebook.react.bridge.ActivityEventListener;
@@ -33,8 +34,6 @@ public class SendSMSModule extends ReactContextBaseJavaModule implements Activit
 
     @Override
     public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-        //System.out.println("in module onActivityResult() request " + requestCode + " result " + resultCode);
-        //canceled intent
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_CANCELED) {
             sendCallback(false, true, false);
         }
@@ -62,41 +61,35 @@ public class SendSMSModule extends ReactContextBaseJavaModule implements Activit
             ReadableArray recipients = options.hasKey("recipients") ? options.getArray("recipients") : null;
 
             Intent sendIntent;
+            String recipientString = "";
+
+            if (recipients != null) {
+                //Samsung for some reason uses commas and not semicolons as a delimiter
+                String separator = ",";
+                if(android.os.Build.MANUFACTURER.equalsIgnoreCase("Samsung")){
+                    separator = ",";
+                }
+
+                for (int i = 0; i < recipients.size(); i++) {
+                    recipientString += recipients.getString(i);
+                    recipientString += separator;
+                }
+            }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                String defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(reactContext);
-                sendIntent = new Intent(Intent.ACTION_SEND);
-                if (defaultSmsPackageName != null){
-                    sendIntent.setPackage(defaultSmsPackageName);
-                }
-                sendIntent.setType("text/plain");
-            }else {
+                sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + recipientString));
+            } else {
                 sendIntent = new Intent(Intent.ACTION_VIEW);
                 sendIntent.setType("vnd.android-dir/mms-sms");
+                sendIntent.putExtra("address", recipientString);
             }
 
             sendIntent.putExtra("sms_body", body);
             sendIntent.putExtra(sendIntent.EXTRA_TEXT, body);
             sendIntent.putExtra("exit_on_sent", true);
 
-            //if recipients specified
-            if (recipients != null) {
-                //Samsung for some reason uses commas and not semicolons as a delimiter
-                String separator = ";";
-                if(android.os.Build.MANUFACTURER.equalsIgnoreCase("Samsung")){
-                    separator = ",";
-                }
-                String recipientString = "";
-                for (int i = 0; i < recipients.size(); i++) {
-                    recipientString += recipients.getString(i);
-                    recipientString += separator;
-                }
-                sendIntent.putExtra("address", recipientString);
-            }
-
             reactContext.startActivityForResult(sendIntent, REQUEST_CODE, sendIntent.getExtras());
         } catch (Exception e) {
-            //error!
             sendCallback(false, false, true);
             throw e;
         }
